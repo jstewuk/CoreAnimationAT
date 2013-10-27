140fcf308faf06a2762f1e48a9706652e895516d
Jim Stewart <jim@stewartstuff.com>
2013-10-26 21:23:25 -0500

    Through 7.3

--- CoreAnimationAT/JSViewController.m
+++ CoreAnimationAT/JSViewController.m
@@ -11,10 +11,8 @@
 
 @interface JSViewController ()
 @property(nonatomic, strong) UIView *layerView;
-@property(nonatomic, strong) UIView *topLeftView;
-@property(nonatomic, strong) UIView *topRightView;
-@property(nonatomic, strong) UIView *bottomLeftView;
-@property(nonatomic, strong) UIView *bottomRightView;
+@property(nonatomic, strong) CALayer *colorLayer;
+
 
 
 @end
@@ -29,23 +27,6 @@
     return self;
 }
 
-- (void)addSpriteImage:(UIImage *)image
-       withContentRect:(CGRect)rect
-               toLayer:(CALayer *)layer
-{
-    layer.contents = (__bridge id)image.CGImage;
-    layer.contentsGravity = kCAGravityResizeAspect;
-    layer.contentsRect = rect;
-}
-
-- (void)addStretchableImage:(UIImage *)image
-          withContentCenter:(CGRect)rect
-                    toLayer:(CALayer *)layer
-{
-    layer.contents = (__bridge id)image.CGImage;
-    layer.contentsCenter = rect;
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor grayColor];
@@ -53,19 +34,56 @@
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20.0, 20.0, 100.0, 50)];
     label.text = @"Some Text";
     [self.view addSubview:label];
-
     
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"Change Color" forState:UIControlStateNormal];
+    [button sizeToFit];
+    button.center = CGPointMake(self.view.bounds.size.width / 2,
+                                self.view.bounds.size.height / 2 + 130);
+    [button addTarget:self action:@selector(changeColor) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+
+    [self addLayerView];
+    
+    [self addColorLayerAsSublayer];
+    
+}
+
+- (void)addLayerView {
     self.layerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
     self.layerView.center = CGPointMake(self.view.bounds.size.width / 2, self.view.bounds.size.height/2);
     self.layerView.backgroundColor = [UIColor yellowColor];
     
-    UIImage *image = [UIImage imageNamed:@"Stretch.png"];
-    
-    CGRect rect = CGRectMake(0.1, 0.1, 0.8, 0.8);
-    [self addStretchableImage:image withContentCenter:rect toLayer:self.layerView.layer];
-    
-    
     [self.view addSubview:self.layerView];
+
+}
+
+- (void)addColorLayerAsSublayer {
+    self.colorLayer = [CALayer layer];
+    self.colorLayer.frame = CGRectMake(50.0, 50.0, 100.0, 100.0);
+    self.colorLayer.backgroundColor = [UIColor blueColor].CGColor;
+    [self.layerView.layer addSublayer:self.colorLayer];
+}
+
+- (void)changeColor {
+    [CATransaction begin];
+    [CATransaction setAnimationDuration:2.0];
+    
+    // WTH does this have to go before the propery animation to make it happen at completion???
+    [CATransaction setCompletionBlock:^{
+        CGAffineTransform transform = self.colorLayer.affineTransform;
+        transform = CGAffineTransformRotate(transform, M_PI_2);
+        self.colorLayer.affineTransform = transform;
+    }];
+    
+    CGFloat red = arc4random() / (CGFloat)INT_MAX;
+    CGFloat green = arc4random() / (CGFloat)INT_MAX;
+    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+    self.colorLayer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0].CGColor;
+    
+    
+    [CATransaction commit];
+    
 }
 
 @end
