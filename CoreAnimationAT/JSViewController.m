db9d62fb33676d146da4e48b2e839420f9e59745
Jim Stewart <jim@stewartstuff.com>
2013-10-27 19:05:16 -0500

    Complete using presentationLayer to determine current layer position.

--- CoreAnimationAT/JSViewController.m
+++ CoreAnimationAT/JSViewController.m
@@ -10,7 +10,6 @@
 #import <QuartzCore/QuartzCore.h>
 
 @interface JSViewController ()
-@property(nonatomic, strong) UIView *layerView;
 @property(nonatomic, strong) CALayer *colorLayer;
 
 
@@ -19,8 +18,7 @@
 
 @implementation JSViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
     }
@@ -31,31 +29,8 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor grayColor];
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20.0, 20.0, 100.0, 50)];
-    label.text = @"Some Text";
-    [self.view addSubview:label];
-    
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
-    [button setTitle:@"Change Color" forState:UIControlStateNormal];
-    [button sizeToFit];
-    button.center = CGPointMake(self.view.bounds.size.width / 2,
-                                self.view.bounds.size.height / 2 + 130);
-    [button addTarget:self action:@selector(changeColor) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:button];
-
-    [self addLayerView];
-    
     [self addColorLayerAsSublayer];
     
-}
-
-- (void)addLayerView {
-    self.layerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
-    self.layerView.center = CGPointMake(self.view.bounds.size.width / 2, self.view.bounds.size.height/2);
-    self.layerView.backgroundColor = [UIColor yellowColor];
-    
-    [self.view addSubview:self.layerView];
-
 }
 
 - (void)addColorLayerAsSublayer {
@@ -68,28 +43,23 @@
     transition.subtype = kCATransitionFromLeft;
     self.colorLayer.actions = @{@"backgroundColor": transition};
     
-    [self.layerView.layer addSublayer:self.colorLayer];
+    [self.view.layer addSublayer:self.colorLayer];
 }
 
-- (void)changeColor {
-    [CATransaction begin];
-    [CATransaction setAnimationDuration:1.0];
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint point = [[touches anyObject] locationInView:self.view];
     
-    // WTH does this have to go before the propery animation to make it happen at completion???
-    [CATransaction setCompletionBlock:^{
-        CGAffineTransform transform = self.colorLayer.affineTransform;
-        transform = CGAffineTransformRotate(transform, M_PI_2);
-        self.colorLayer.affineTransform = transform;
-    }];
-    
-    CGFloat red = arc4random() / (CGFloat)INT_MAX;
-    CGFloat green = arc4random() / (CGFloat)INT_MAX;
-    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
-    self.colorLayer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0].CGColor;
-    
-    
-    [CATransaction commit];
-    
+    if ([self.colorLayer.presentationLayer hitTest:point]) {
+        CGFloat red = arc4random() / (CGFloat)INT_MAX;
+        CGFloat green = arc4random() / (CGFloat)INT_MAX;
+        CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+        self.colorLayer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0].CGColor;
+    } else {
+        [CATransaction begin];
+        [CATransaction setAnimationDuration:4.0];
+        self.colorLayer.position = point;
+        [CATransaction commit];
+    }
 }
 
 @end
